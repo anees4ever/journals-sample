5379b001f285babc9f1a2cf59299e0a28c774061
Muhammed Anees C.A <anees4ever@gmail.com>
2023-10-09 03:29:45 +0530

    Included MySQL Dump file

---
+++ orionedge_hexa_erp.sql
@@ -0,0 +1,378 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Oct 08, 2023 at 11:58 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `orionedge_hexa_erp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account_heads`
+--
+
+CREATE TABLE `account_heads` (
+  `id` int(11) NOT NULL,
+  `account_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `account_heads`
+--
+
+INSERT INTO `account_heads` (`id`, `account_name`) VALUES
+(1, 'EAL - Corporate Division'),
+(2, 'FieldCore Services Solutions');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `companies`
+--
+
+CREATE TABLE `companies` (
+  `id` int(11) NOT NULL,
+  `company_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `companies`
+--
+
+INSERT INTO `companies` (`id`, `company_name`) VALUES
+(1, 'Eram Arabia Ltd'),
+(2, 'Orion Edge');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cost_centers`
+--
+
+CREATE TABLE `cost_centers` (
+  `id` int(11) NOT NULL,
+  `cost_center_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cost_centers`
+--
+
+INSERT INTO `cost_centers` (`id`, `cost_center_name`) VALUES
+(1, 'General P & C'),
+(2, 'SAUDIKAD');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cost_types`
+--
+
+CREATE TABLE `cost_types` (
+  `id` int(11) NOT NULL,
+  `type_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cost_types`
+--
+
+INSERT INTO `cost_types` (`id`, `type_name`) VALUES
+(1, 'Project'),
+(2, 'Service');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `departments`
+--
+
+CREATE TABLE `departments` (
+  `id` int(11) NOT NULL,
+  `department_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `departments`
+--
+
+INSERT INTO `departments` (`id`, `department_name`) VALUES
+(1, 'FINANCE'),
+(2, 'SALES');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `divisions`
+--
+
+CREATE TABLE `divisions` (
+  `id` int(11) NOT NULL,
+  `division_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `divisions`
+--
+
+INSERT INTO `divisions` (`id`, `division_name`) VALUES
+(1, 'P&C Division'),
+(2, 'Accounts'),
+(3, 'Sales & Promotions'),
+(4, 'IT Division');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice_types`
+--
+
+CREATE TABLE `invoice_types` (
+  `id` int(11) NOT NULL,
+  `type_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `invoice_types`
+--
+
+INSERT INTO `invoice_types` (`id`, `type_name`) VALUES
+(1, 'New Ref'),
+(2, 'Service Bill');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `journals`
+--
+
+CREATE TABLE `journals` (
+  `id` int(11) NOT NULL,
+  `voucher_no` varchar(12) NOT NULL,
+  `voucher_date` date NOT NULL,
+  `cashbook_affected` enum('T','F') NOT NULL DEFAULT 'F',
+  `voucher_ref_no` varchar(12) NOT NULL,
+  `company_id` int(11) NOT NULL,
+  `division_id` int(11) NOT NULL,
+  `department_id` int(11) NOT NULL,
+  `voucher_amt` double(23,2) NOT NULL DEFAULT 0.00,
+  `voucher_narr` text NOT NULL,
+  `voucher_narr_arabic` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `transaction_data` text NOT NULL,
+  `cost_center_data` text NOT NULL,
+  `invoice_data` text NOT NULL,
+  `created_at` datetime NOT NULL,
+  `modified_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `journals_cost_centers`
+--
+
+CREATE TABLE `journals_cost_centers` (
+  `id` int(11) NOT NULL,
+  `trans_id` int(11) NOT NULL,
+  `cost_type` int(11) NOT NULL,
+  `cost_center_code` varchar(20) NOT NULL,
+  `cost_center_id` int(11) NOT NULL,
+  `cost_amount` double(23,2) NOT NULL DEFAULT 0.00,
+  `remarks` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `journals_invoices`
+--
+
+CREATE TABLE `journals_invoices` (
+  `id` int(11) NOT NULL,
+  `cost_center_id` int(11) NOT NULL,
+  `invoice_type` int(11) NOT NULL,
+  `invoice_no` varchar(100) NOT NULL,
+  `invoice_date` date NOT NULL,
+  `invoice_amount` double(23,2) NOT NULL DEFAULT 0.00
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `journals_trans`
+--
+
+CREATE TABLE `journals_trans` (
+  `id` int(11) NOT NULL,
+  `journal_id` int(11) NOT NULL,
+  `trans_type` enum('Dr','Cr') NOT NULL,
+  `account_code` text NOT NULL,
+  `account_id` int(11) NOT NULL,
+  `trans_amount` double(23,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `account_heads`
+--
+ALTER TABLE `account_heads`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `companies`
+--
+ALTER TABLE `companies`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cost_centers`
+--
+ALTER TABLE `cost_centers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cost_types`
+--
+ALTER TABLE `cost_types`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `departments`
+--
+ALTER TABLE `departments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `divisions`
+--
+ALTER TABLE `divisions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `invoice_types`
+--
+ALTER TABLE `invoice_types`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `journals`
+--
+ALTER TABLE `journals`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `idxVoucherNumber` (`voucher_no`),
+  ADD KEY `idxCompany` (`company_id`),
+  ADD KEY `idxAmount` (`voucher_amt`),
+  ADD KEY `idxDivision` (`division_id`),
+  ADD KEY `idxDepartment` (`department_id`);
+
+--
+-- Indexes for table `journals_cost_centers`
+--
+ALTER TABLE `journals_cost_centers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `journals_invoices`
+--
+ALTER TABLE `journals_invoices`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `journals_trans`
+--
+ALTER TABLE `journals_trans`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `account_heads`
+--
+ALTER TABLE `account_heads`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `companies`
+--
+ALTER TABLE `companies`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `cost_centers`
+--
+ALTER TABLE `cost_centers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `cost_types`
+--
+ALTER TABLE `cost_types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `departments`
+--
+ALTER TABLE `departments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `divisions`
+--
+ALTER TABLE `divisions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `invoice_types`
+--
+ALTER TABLE `invoice_types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `journals`
+--
+ALTER TABLE `journals`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `journals_cost_centers`
+--
+ALTER TABLE `journals_cost_centers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `journals_invoices`
+--
+ALTER TABLE `journals_invoices`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `journals_trans`
+--
+ALTER TABLE `journals_trans`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
